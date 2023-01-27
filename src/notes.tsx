43151969c503f1d592da44b3103bf4e11453c13b
Kapil Pagani <kapil.pagani@gmail.com>
2023-01-27 11:45:33 +0530

    Added notes

--- src/notes.tsx
+++ src/notes.tsx
@@ -13,3 +13,7 @@
 // https://dev.to/ivadyhabimana/setup-jest-and-react-testing-library-in-a-react-project-a-step-by-step-guide-1mf0
 
 // https://codeburst.io/how-to-use-a-test-reporter-with-create-react-app-6c779f71f62
+
+// git config --global user.email "you@example.com"
+//   git config --global user.name "Your Name"
+
